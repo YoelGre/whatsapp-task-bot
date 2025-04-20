50ca762d7b86f7c20014401b2f20784f66ecedf1
YoelGre <yoel.greenberg@mail.huji.ac.il>
2025-04-20 14:56:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -95,7 +95,8 @@ reminder_thread = Thread(target=reminder_loop, daemon=True)
 reminder_thread.start()
 
 if __name__ == "__main__":
-import os
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
